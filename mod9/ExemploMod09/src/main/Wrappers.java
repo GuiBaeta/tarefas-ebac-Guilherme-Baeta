4fb320f9f4466ef787e0a09cd96a011c69c2b9e9
GuiBaeta <guilhermebaetapena@gmail.com>
2024-03-22 17:46:59 -0300

    exemplo m9

---
+++ mod9/ExemploMod09/src/main/Wrappers.java
@@ -0,0 +1,23 @@
+package main;
+
+public class Wrappers {
+
+	public Integer codigo=1;
+	private Long codigoMaior=123456789l;
+	private Double valorDecimal1=10.1;
+	private Boolean status =false;
+	private Float valorDecimal=10.0f;
+	private Short shor;
+	private Byte bi;
+	private Character letra = 'A';
+	
+	
+	public static void main(String args[]) {
+		
+	}
+	
+	public void test() {
+		
+	}
+	
+}
